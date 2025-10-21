ac6507999d9466d0f6581ac1d7e0166389a9ce15
Choi HongCheol <choisieun89@gmail.com>
2025-10-21 13:01:27 +0900

    fix: 폰트 변경 및 큰 폰트 짤림증상 수정

--- src/app/auth/login/page.tsx
+++ src/app/auth/login/page.tsx
@@ -2,11 +2,23 @@ import GoogleLoginButton from "./_/components/GoogleLoginButton";
 
 async function LoginPage() {
   return (
-    <div className="flex h-[100dvh] w-[100dvw] flex-col items-center justify-center text-center">
-      <h1 className="relative mb-5 inline-block bg-gradient-to-r from-blue-800 to-sky-200 bg-clip-text text-5xl font-black tracking-tight text-transparent">
-        DevHong 이미지 백업서버
-      </h1>
+    <div className="flex h-[100dvh] w-[100dvw] flex-col items-center justify-center pb-[60%] text-center">
+      <p className="relative inline-block bg-gradient-to-r from-blue-800 to-sky-200 bg-clip-text text-5xl leading-normal font-black tracking-tight text-transparent">
+        DevHong
+      </p>
+      <p className="leading-auto text-5xl leading-none text-black">📸</p>
+      <p className="relative mb-7 inline-block bg-gradient-to-r from-sky-200 to-blue-800 bg-clip-text text-5xl leading-normal font-black tracking-tight text-transparent">
+        Backup Server
+      </p>
+
       <GoogleLoginButton />
+
+      <p className="mt-8 text-xs leading-relaxed text-gray-600 dark:text-gray-400">
+        ⚠️ 이 서비스는 개인 백업용입니다.
+        <br />
+        해킹, 무단 접근, 불법적인 사용 시<br />
+        법적 책임을 질 수 있습니다.
+      </p>
     </div>
   );
 }
